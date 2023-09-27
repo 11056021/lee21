26d64720f3287820939b222d6ada2bad6011df3c
11056021 <11056021@ntub.edu.tw>
2023-09-27 15:53:29 +0800

    0000000

--- mblog0927/settings.py
+++ mblog0927/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m7r$a0n2(lq_eo!p!6akg1+h64xett63a3iq2x_mdq7pqx6v56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ''
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
