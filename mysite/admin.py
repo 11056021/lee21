7dce0e3bd3b6e57232ccc581c749045ab7b48285
wonwooooo <11056021@ntub.edu.tw>
2023-10-04 15:51:59 +0800

    新增post物件 管理介面

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from mysite.models import Post
 # Register your models here.
+
+#放入細項
+class PostAdmin(admin.ModelAdmin):
+    list_display  = ('title','slug','pub_date')
+
+#將上面新增的細項class加入
+admin.site.register(Post,PostAdmin)
